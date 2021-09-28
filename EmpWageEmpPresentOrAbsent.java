c006ba48b057003d08fdccfe7dc6a8ebe83697ac
unknown <gayoyawalkar@gmail.com>
2021-09-28 19:05:42 +0530

    Added:EmpWageEmpPresentOrAbsent file Uc1

---
+++ EmpWageEmpPresentOrAbsent.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeewage;
+//UC1
+public class EmpWageEmpPresentOrAbsent {
+	public static void main(String[] args) {
+		int is_full_time = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if (is_full_time == empCheck) {
+			System.out.println("Employee is Present=");
+		} else {
+			System.out.println("Employee is Absent");
+		}		
+	}
+}
